5dc0c8ac86061f0d5118730b70a88b6a632f7a46
Danila2221 <Penny@HOME-PC>
2017-08-28 19:44:11 +0700

    test

--- DarkSouls/Items/Berserker nightmare.cs
+++ DarkSouls/Items/Berserker nightmare.cs
@@ -16,22 +16,24 @@ namespace DarkSouls.Items
         
         public override void SetDefaults()
         {
-            item.width = 34;
-            item.height = 32;
-            
-            item.useAnimation = 44;
-            item.useTime = 44;
             item.maxStack = 1;
             item.damage = 100;
-            item.knockBack = 12;
-            item.scale = 1.1f;
-            item.UseSound = SoundID.Item1;
+            item.useStyle = 5;
+            item.useAnimation = 44;
+            item.useTime = 44;
+            item.shootSpeed = 8f;
+            item.knockBack = 9.5f;
+            item.width = 32;
+            item.height = 32;
+            item.scale = 1f;
             item.rare = 5;
-            item.shootSpeed = 12;
-            item.noMelee = true;
-            item.value = 505000;
-            
+            item.UseSound = SoundID.Item1;
             item.shoot = mod.ProjectileType<BerserkerNightmareP>();
+            item.value = 27000;
+            item.noMelee = true; // Important because the spear is actually a projectile instead of an item. This prevents the melee hitbox of this item.
+            item.noUseGraphic = true; // Important, it's kind of wired if people see two spears at one time. This prevents the melee animation of this item.
+            item.melee = true;
+            item.autoReuse = true; // Most spears don't autoReuse, but it's possible
         }
 
 
