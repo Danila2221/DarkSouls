f5bd98eb1cb9d3f5966fcc02839fe33d7220cb77
Danila2221 <Penny@HOME-PC>
2017-08-28 20:50:29 +0700

    bug fix

--- DarkSouls/Items/Berserker nightmare2.cs
+++ DarkSouls/Items/Berserker nightmare2.cs
@@ -25,8 +25,8 @@ namespace DarkSouls.Items
             item.maxStack = 1;
             item.damage = 250;
             item.useStyle = 5;
-            item.useAnimation = 60;
-            item.useTime = 60;
+            item.useAnimation = 20;
+            item.useTime = 20;
             item.shootSpeed = 60;
             item.knockBack = 9.5f;
             item.width = 32;
