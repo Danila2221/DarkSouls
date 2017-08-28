d54aca31feec11d9a35d07ba6c9323258eb3a757
Danila2221 <Penny@HOME-PC>
2017-08-28 14:16:34 +0700

    new item

---
+++ DarkSouls/Items/AttraidiesRelic.cs
@@ -0,0 +1,52 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DarkSouls.Items
+{
+
+    public class AttraidiesRelic : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Summons a mindflayer illusion from the legion army of Attraidies!"
+                + "\nDrops 600 souls of utterly corrupt darkness, among other random things."
+                + "\nYou feel compelled to try this...");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 34;
+            item.height = 40;
+            item.maxStack = 1;
+            item.rare = 9;
+            item.useAnimation = 45;
+            item.useTime = 55;
+            item.useStyle = 4;
+            item.UseSound = SoundID.Item44;
+            item.value = 1000;
+            item.consumable = false;
+        }
+
+        // We use the CanUseItem hook to prevent a player from using this item while the boss is present in the world.
+        public override bool CanUseItem(Player player)
+        {
+            // "player.ZoneUnderworldHeight" could also be written as "player.position.Y / 16f > Main.maxTilesY - 200"
+            return !NPC.AnyNPCs(mod.NPCType("MindflayerIllusion"));
+        }
+
+        public override bool UseItem(Player player)
+        {
+            
+                if (Main.netMode != 1)
+                {
+                NPC.SpawnOnPlayer(player.whoAmI, mod.NPCType("MindflayerIllusion"));
+                }
+                
+            
+            return true;
+        }
+
+        
+    }
+}
---
+++ DarkSouls/Items/BandofCosmicPower.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.World.Generation;
+using Microsoft.Xna.Framework;
+using Terraria.GameContent.Generation;
+using Terraria.ModLoader.IO;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using DarkSouls;
+
+namespace DarkSouls.Items
+{
+    public class BandofCosmicPower : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Band of Cosmic Power");
+            Tooltip.SetDefault("+ 2 life regeneration and increases max mana by 40"
+                + "\nCan be upgraded with 10,000 Dark Souls");
+        }
+
+        public override void SetDefaults()
+        {
+
+            item.width = 22;
+            item.height = 22;
+            item.accessory = true;
+
+            item.value = 5000;
+            item.rare = 1;
+            item.maxStack = 1;
+
+        }
+        public override void UpdateEquip(Player player)
+        {
+            player.lifeRegen += 2;
+            player.statManaMax2 += 40;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.BandofRegeneration, 1);
+            recipe.AddIngredient(ItemID.BandofStarpower, 1);
+            recipe.AddIngredient(null, "DarkSoul", 3000);
+            recipe.SetResult(this);
+            recipe.AddTile(26);
+            recipe.AddRecipe();
+        }
+    }
+}
+
+ 
+    
+    
+
