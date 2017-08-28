1de3fc75151905f05b0f5b44f21a74708497dbf0
Danila2221 <Penny@HOME-PC>
2017-08-28 19:38:41 +0700

    Revert "test"
    This reverts commit 07da9305e2eb5519f82cba875aba9afa34f760df.

---
+++ DarkSouls/Items/Berserker nightmare.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using DarkSouls.Projectiles;
+
+namespace DarkSouls.Items
+{
+    public class BerserkerNightmare : ModItem
+    {
+        
+        public override void SetDefaults()
+        {
+            item.width = 34;
+            item.height = 32;
+            item.channel = true;
+            item.useAnimation = 44;
+            item.useTime = 44;
+            item.maxStack = 1;
+            item.damage = 100;
+            item.knockBack = 12;
+            item.scale = 1.1f;
+            item.UseSound = SoundID.Item1;
+            item.rare = 5;
+            item.shootSpeed = 12;
+            item.noUseGraphic = true;
+            item.noMelee = true;
+            item.value = 505000;
+            item.melee = true;
+            item.shoot = mod.ProjectileType<BerserkerNightmareP>();
+        }
+
+
+    }
+}
