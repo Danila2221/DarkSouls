971f3ffe4f58da0c0f3483adca586da5ab9a9251
Danila2221 <Penny@HOME-PC>
2017-08-28 21:09:08 +0700

    new item

---
+++ DarkSouls/Items/Beserker Emblem.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.World.Generation;
+using Microsoft.Xna.Framework;
+using Terraria.GameContent.Generation;
+using Terraria.ModLoader.IO;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using DarkSouls;
+
+namespace DarkSouls.Items
+{
+    public class BerserkerEmblem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Berserker Emblem");
+            Tooltip.SetDefault("25% increased melee damage");
+        }
+
+        public override void SetDefaults()
+        {
+
+            item.width = 24;
+            item.height = 24;
+            item.accessory = true;
+
+            item.value = 100000;
+            item.rare = 10;
+            item.maxStack = 1;
+
+        }
+        public override void UpdateEquip(Player player)
+        {
+            player.meleeDamage += 0.25f;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.WarriorEmblem, 1);
+            recipe.AddIngredient(ItemID.SoulofLight, 5);
+            recipe.AddIngredient(ItemID.SoulofNight, 5);
+            recipe.AddIngredient(ItemID.SoulofSight, 1);
+
+            recipe.AddIngredient(null, "DarkSoul", 5000);
+            recipe.SetResult(this);
+            recipe.AddTile(26);
+            recipe.AddRecipe();
+        }
+    }
+}
