53d3351a92e09b61590ae9821da304a482a82161
Danila2221 <Penny@HOME-PC>
2017-08-30 16:38:32 +0700

    newitem

---
+++ DarkSouls/Items/Blight Stone.cs
@@ -0,0 +1,66 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DarkSouls.Items
+{
+	
+	public class BlightStone : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("Summons the Abysmal Oolacile Sorcerer.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 28;
+			item.height = 28;
+			item.maxStack = 1;
+			item.rare = 9;
+			item.useAnimation = 45;
+			item.useTime = 55;
+			item.useStyle = 4;
+			item.UseSound = SoundID.Item44;
+			item.value = 700000;
+			item.consumable = false;
+		}
+
+		// We use the CanUseItem hook to prevent a player from using this item while the boss is present in the world.
+		public override bool CanUseItem(Player player)
+		{
+			// "player.ZoneUnderworldHeight" could also be written as "player.position.Y / 16f > Main.maxTilesY - 200"
+			return !NPC.AnyNPCs(mod.NPCType("Blight"));
+		}
+
+		public override bool UseItem(Player player)
+		{
+            if (Main.bloodMoon == true)
+            {
+                Main.NewText("The Blight cannot be summoned or fought while in the Abyss...", 175, 75, 255);
+            }
+            if (Main.bloodMoon == false)
+            {
+                if (Main.netMode != 1)
+                {
+                NPC.NewNPC((int)player.position.X - 700, (int)player.position.Y - 500, mod.NPCType("Blight"), 1);
+                }
+                    Main.NewText("You will be destroyed");
+            }
+           
+
+			return true;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(null, "RedTitanite", 10);
+			recipe.AddIngredient(null, "WhiteTitanite", 5);
+			recipe.AddIngredient(null, "DarkSoul", 1000);
+			recipe.SetResult(this);
+			recipe.AddTile(26);
+			recipe.AddRecipe();
+		}
+	}
+}
--- DarkSouls/NPCs/Enemies/TheEarthFiendLich.cs
+++ DarkSouls/NPCs/Enemies/TheEarthFiendLich.cs
@@ -35,7 +35,7 @@ namespace DarkSouls.NPCs.Enemies
         {
             DisplayName.SetDefault("Lich, the Earth Fiend");
 
-            Main.npcFrameCount[npc.type] = 1;
+            Main.npcFrameCount[npc.type] = 8;
         }
         public override void SetDefaults()
         {
