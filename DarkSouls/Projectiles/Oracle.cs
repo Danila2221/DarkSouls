8ccf7f00f493aac0f256543ea3c8e8a199cfef36
Danila2221 <Penny@HOME-PC>
2017-08-31 15:32:57 +0700

    test

--- DarkSouls/Projectiles/Oracle.cs
+++ DarkSouls/Projectiles/Oracle.cs
@@ -32,9 +32,9 @@ namespace DarkSouls.Projectiles
                 projectile.velocity.Y *= 1.01f;
 
                 Rectangle projrec = new Rectangle((int)projectile.position.X + (int)projectile.velocity.X, (int)projectile.position.Y + (int)projectile.velocity.Y, projectile.width, projectile.height);
-            Rectangle prec = new Rectangle((int)Main.player[Main.myPlayer].position.X, (int)Main.player[Main.myPlayer].position.Y, (int)Main.player[Main.myPlayer].width, (int)Main.player[Main.myPlayer].height);
+                Rectangle prec = new Rectangle((int)Main.player[Main.myPlayer].position.X, (int)Main.player[Main.myPlayer].position.Y, (int)Main.player[Main.myPlayer].width, (int)Main.player[Main.myPlayer].height);
 
-            
+            }
 
         }
 
