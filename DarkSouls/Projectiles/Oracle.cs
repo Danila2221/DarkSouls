1cff71b4496f56c12a5aff18f3c72028beb1a342
Danila2221 <Penny@HOME-PC>
2017-08-31 15:32:39 +0700

    test

--- DarkSouls/Projectiles/Oracle.cs
+++ DarkSouls/Projectiles/Oracle.cs
@@ -18,35 +18,23 @@ namespace DarkSouls.Projectiles
             projectile.scale = 1;
             projectile.tileCollide = false;
             projectile.width = 16;
-            projectile.timeLeft = 250;
+            projectile.timeLeft = 2500;
         }
         public override void AI()
         {
-            projectile.rotation += 1f;
-            if (Main.rand.Next(4) == 0)
-            {
-                int dust = Dust.NewDust(new Vector2((float)projectile.position.X, (float)projectile.position.Y), projectile.width, projectile.height, 6, 0, 0, 50, Color.Green, 3.0f);
-                Main.dust[dust].noGravity = false;
-            }
-            Lighting.AddLight((int)(projectile.position.X / 16f), (int)(projectile.position.Y / 16f), 0.4f, 0.1f, 0.1f);
+            projectile.rotation++;
+            int dust = Dust.NewDust(new Vector2((float)projectile.position.X + 10, (float)projectile.position.Y), projectile.width, projectile.height, 6, 0, 0, 200, Color.Red, 1f);
+            Main.dust[dust].noGravity = true;
 
-            if (projectile.velocity.X <= 4 && projectile.velocity.Y <= 4 && projectile.velocity.X >= -4 && projectile.velocity.Y >= -4)
+            if (projectile.velocity.X <= 10 && projectile.velocity.Y <= 10 && projectile.velocity.X >= -10 && projectile.velocity.Y >= -10)
             {
-                float accel = 1f + 30;
-                projectile.velocity.X *= accel;
-                projectile.velocity.Y *= accel;
-            }
+                projectile.velocity.X *= 1.01f;
+                projectile.velocity.Y *= 1.01f;
 
-            Rectangle projrec = new Rectangle((int)projectile.position.X + (int)projectile.velocity.X, (int)projectile.position.Y + (int)projectile.velocity.Y, projectile.width, projectile.height);
+                Rectangle projrec = new Rectangle((int)projectile.position.X + (int)projectile.velocity.X, (int)projectile.position.Y + (int)projectile.velocity.Y, projectile.width, projectile.height);
             Rectangle prec = new Rectangle((int)Main.player[Main.myPlayer].position.X, (int)Main.player[Main.myPlayer].position.Y, (int)Main.player[Main.myPlayer].width, (int)Main.player[Main.myPlayer].height);
 
-            if (projrec.Intersects(prec))
-            {
-                Main.player[Main.myPlayer].AddBuff(13, 600, true);
-                Main.player[Main.myPlayer].AddBuff(36, 300, false);
-                Main.player[Main.myPlayer].AddBuff(30, 7200, false);
-                Main.player[Main.myPlayer].AddBuff(20, 3600, false);
-            }
+            
 
         }
 
