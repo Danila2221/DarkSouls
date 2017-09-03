10a2a2104a4a8685057a72d531020756649f3b45
Danila2221 <Penny@HOME-PC>
2017-09-03 18:49:09 +0700

    new items

---
+++ DarkSouls/Items/Blue Titanite.cs
@@ -0,0 +1,29 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+ 
+namespace DarkSouls.Items
+{
+    public class BlueTitanite : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            
+        }
+        public override void SetDefaults()
+        {
+            
+            item.width = 16;
+            item.height = 16;
+            item.value = 200000;
+            item.rare = 4;
+            item.maxStack = 250;
+            
+        }
+ 
+        
+ 
+        
+    }
+}
---
+++ DarkSouls/Items/Boots speed.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DarkSouls.Items
+{
+	public class BootsofHaste : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+            DisplayName.SetDefault("Boots of Haste");
+            Tooltip.SetDefault("Adds 20% speed.");
+			
+		}
+		public override void SetDefaults()
+		{
+			
+			item.width = 32;
+			item.height = 26;
+            item.maxStack = 1;
+            item.value = 30000;
+			item.rare = 3;
+			item.accessory = true;
+		}
+
+		public override void UpdateAccessory(Player player, bool hideVisual)
+		{
+            player.moveSpeed += 0.2f;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.HermesBoots, 1);
+            recipe.AddIngredient(null, "DarkSoul", 2);
+            recipe.SetResult(this);
+            recipe.AddTile(26);
+            recipe.AddRecipe();
+        }
+
+
+    }
+}
