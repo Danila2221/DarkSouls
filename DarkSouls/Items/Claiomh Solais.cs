7d8614b69647fb85afc4c8b4296ee2503d65b223
Danila2221 <Penny@HOME-PC>
2017-09-04 20:00:37 +0700

    bug fix

--- DarkSouls/Items/Claiomh Solais.cs
+++ DarkSouls/Items/Claiomh Solais.cs
@@ -30,6 +30,13 @@ namespace DarkSouls.Items
             item.value = 300000;
             item.melee = true;
         }
+        public override void MeleeEffects(Player player, Rectangle rectangle)
+        {
+            Color color = new Color();
+            //This is the same general effect done with the Fiery Greatsword
+            int dust = Dust.NewDust(new Vector2((float)rectangle.X, (float)rectangle.Y), rectangle.Width, rectangle.Height, 15, (player.velocity.X * 0.2f) + (player.direction * 3), player.velocity.Y * 0.2f, 100, color, 1.0f);
+            Main.dust[dust].noGravity = true;
+        }
         public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
