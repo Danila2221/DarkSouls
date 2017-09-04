b5a626d1fee38bfd182b2ea01f87d8a813b00846
Danila2221 <Penny@HOME-PC>
2017-09-04 20:06:57 +0700

    new ai testing

--- DarkSouls/Projectiles/CelestialLance.cs
+++ DarkSouls/Projectiles/CelestialLance.cs
@@ -25,42 +25,72 @@ namespace DarkSouls.Projectiles
 
         public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
         {
-            
+
             Projectile.NewProjectile(target.position.X - 0, target.position.Y - 100, 0, 5, ProjectileID.FallingStar, 500, 1f, Main.myPlayer);
         }
-
+        public float movementFactor // Change this value to alter how fast the spear moves
+        {
+            get { return projectile.ai[0]; }
+            set { projectile.ai[0] = value; }
+        }
 
         public override void AI()
         {
             Lighting.AddLight((int)((projectile.position.X + (float)projectile.width) / 16f), (int)((projectile.position.Y + (float)(projectile.height / 2)) / 16f), 0.8f, 0.7f, 0.1f);
-            Main.player[projectile.owner].direction = projectile.direction;
-            Main.player[projectile.owner].heldProj = projectile.whoAmI;
-            Main.player[projectile.owner].itemTime = Main.player[projectile.owner].itemAnimation;
-            projectile.position.X = Main.player[projectile.owner].position.X + (float)(Main.player[projectile.owner].width / 2) - (float)(projectile.width / 2);
-            projectile.position.Y = Main.player[projectile.owner].position.Y + (float)(Main.player[projectile.owner].height / 2) - (float)(projectile.height / 2);
-            projectile.position += projectile.velocity * projectile.ai[0]; if (projectile.ai[0] == 0f)
+            Player projOwner = Main.player[projectile.owner];
+            // Here we set some of the projectile's owner properties, such as held item and itemtime, along with projectile direction and position based on the player
+            Vector2 ownerMountedCenter = projOwner.RotatedRelativePoint(projOwner.MountedCenter, true);
+            projectile.direction = projOwner.direction;
+            projOwner.heldProj = projectile.whoAmI;
+            projOwner.itemTime = projOwner.itemAnimation;
+            projectile.position.X = ownerMountedCenter.X - (float)(projectile.width / 2);
+            projectile.position.Y = ownerMountedCenter.Y - (float)(projectile.height / 2);
+            // As long as the player isn't frozen, the spear can move
+            if (!projOwner.frozen)
             {
-                projectile.ai[0] = 3f;
-                projectile.netUpdate = true;
+                if (movementFactor == 0f) // When initially thrown out, the ai0 will be 0f
+                {
+                    movementFactor = 3f; // Make sure the spear moves forward when initially thrown out
+                    projectile.netUpdate = true; // Make sure to netUpdate this spear
+                }
+                if (projOwner.itemAnimation < projOwner.itemAnimationMax / 3) // Somewhere along the item animation, make sure the spear moves back
+                {
+                    movementFactor -= 2.4f;
+                }
+                else // Otherwise, increase the movement factor
+                {
+                    movementFactor += 2.1f;
+                }
             }
-            if (Main.player[projectile.owner].itemAnimation < Main.player[projectile.owner].itemAnimationMax / 3)
-            {
-                projectile.ai[0] -= 1.1f;
-            }
-            else
-            {
-                projectile.ai[0] += 0.95f;
-            }
-
-            if (Main.player[projectile.owner].itemAnimation == 0)
+            // Change the spear position based off of the velocity and the movementFactor
+            projectile.position += projectile.velocity * movementFactor;
+            // When we reach the end of the animation, we can kill the spear projectile
+            if (projOwner.itemAnimation == 0)
             {
                 projectile.Kill();
             }
-
-            projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 2.355f;
+            // Apply proper rotation, with an offset of 135 degrees due to the sprite's rotation, notice the usage of MathHelper, use this class!
+            // MathHelper.ToRadians(xx degrees here)
+            projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + MathHelper.ToRadians(135f);
+            // Offset by 90 degrees here
             if (projectile.spriteDirection == -1)
             {
-                projectile.rotation -= 1.57f;
+                projectile.rotation -= MathHelper.ToRadians(90f);
+            }
+
+            // These dusts are added later, for the 'ExampleMod' effect
+            if (Main.rand.Next(3) == 0)
+            {
+                int dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, mod.DustType<Dusts.Sparkle>(), projectile.velocity.X * 0.2f, projectile.velocity.Y * 0.2f, 200, default(Color), 1.2f);
+                Main.dust[dustIndex].velocity += projectile.velocity * 0.3f;
+                Main.dust[dustIndex].velocity *= 0.2f;
+            }
+            if (Main.rand.Next(4) == 0)
+            {
+                int dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, mod.DustType<Dusts.Smoke>(), 0f, 0f, 254, default(Color), 0.3f);
+                Main.dust[dustIndex].velocity += projectile.velocity * 0.5f;
+                Main.dust[dustIndex].velocity *= 0.5f;
+                return;
             }
         }
     }
