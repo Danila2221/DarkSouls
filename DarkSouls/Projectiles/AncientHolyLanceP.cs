fa3ff96ff74ec7f371da1f68a47e84d7d5156934
Danila2221 <Penny@HOME-PC>
2017-09-04 20:17:48 +0700

    new ai

--- DarkSouls/Projectiles/AncientHolyLanceP.cs
+++ DarkSouls/Projectiles/AncientHolyLanceP.cs
@@ -31,39 +31,54 @@ namespace DarkSouls.Projectiles
             projectile.tileCollide = false;
         }
 
-
+        public float movementFactor // Change this value to alter how fast the spear moves
+        {
+            get { return projectile.ai[0]; }
+            set { projectile.ai[0] = value; }
+        }
 
 
         public override void AI()
         {
-            Main.player[projectile.owner].direction = projectile.direction;
-            Main.player[projectile.owner].heldProj = projectile.whoAmI;
-            Main.player[projectile.owner].itemTime = Main.player[projectile.owner].itemAnimation;
-            projectile.position.X = Main.player[projectile.owner].position.X + (float)(Main.player[projectile.owner].width / 2) - (float)(projectile.width / 2);
-            projectile.position.Y = Main.player[projectile.owner].position.Y + (float)(Main.player[projectile.owner].height / 2) - (float)(projectile.height / 2);
-            projectile.position += projectile.velocity * projectile.ai[0]; if (projectile.ai[0] == 0f)
+            Player projOwner = Main.player[projectile.owner];
+            // Here we set some of the projectile's owner properties, such as held item and itemtime, along with projectile direction and position based on the player
+            Vector2 ownerMountedCenter = projOwner.RotatedRelativePoint(projOwner.MountedCenter, true);
+            projectile.direction = projOwner.direction;
+            projOwner.heldProj = projectile.whoAmI;
+            projOwner.itemTime = projOwner.itemAnimation;
+            projectile.position.X = ownerMountedCenter.X - (float)(projectile.width / 2);
+            projectile.position.Y = ownerMountedCenter.Y - (float)(projectile.height / 2);
+            // As long as the player isn't frozen, the spear can move
+            if (!projOwner.frozen)
             {
-                projectile.ai[0] = 3f;
-                projectile.netUpdate = true;
+                if (movementFactor == 0f) // When initially thrown out, the ai0 will be 0f
+                {
+                    movementFactor = 3f; // Make sure the spear moves forward when initially thrown out
+                    projectile.netUpdate = true; // Make sure to netUpdate this spear
+                }
+                if (projOwner.itemAnimation < projOwner.itemAnimationMax / 3) // Somewhere along the item animation, make sure the spear moves back
+                {
+                    movementFactor -= 2.4f;
+                }
+                else // Otherwise, increase the movement factor
+                {
+                    movementFactor += 2.1f;
+                }
             }
-            if (Main.player[projectile.owner].itemAnimation < Main.player[projectile.owner].itemAnimationMax / 3)
-            {
-                projectile.ai[0] -= 1.1f;
-            }
-            else
-            {
-                projectile.ai[0] += 0.95f;
-            }
-
-            if (Main.player[projectile.owner].itemAnimation == 0)
+            // Change the spear position based off of the velocity and the movementFactor
+            projectile.position += projectile.velocity * movementFactor;
+            // When we reach the end of the animation, we can kill the spear projectile
+            if (projOwner.itemAnimation == 0)
             {
                 projectile.Kill();
             }
-
-            projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 2.355f;
+            // Apply proper rotation, with an offset of 135 degrees due to the sprite's rotation, notice the usage of MathHelper, use this class!
+            // MathHelper.ToRadians(xx degrees here)
+            projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + MathHelper.ToRadians(135f);
+            // Offset by 90 degrees here
             if (projectile.spriteDirection == -1)
             {
-                projectile.rotation -= 1.57f;
+                projectile.rotation -= MathHelper.ToRadians(90f);
             }
         }
 
